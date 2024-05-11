1a65bb1d972e703a82a53d65df1f7ceb36ec913f
pcbassani <paloma_bassani@outlook.com>
2024-05-11 12:02:51 -0300

    Ajuste navbar js

--- codigo-fonte/assets/scripts/navbardev.js
+++ codigo-fonte/assets/scripts/navbardev.js
@@ -2,9 +2,9 @@ function menuShow( ) {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/codigo-fonte/assets/img/menu_icon_linhas.svg";
+        document.querySelector('.icon').src = "../../img/menu_icon_linhas.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/codigo-fonte/assets/img/X.svg";
+        document.querySelector('.icon').src = "../../img/X.svg";
     }
 }
